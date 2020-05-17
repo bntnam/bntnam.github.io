e693c2ee952ad9aaaa0d63cedec59dcbd9a7104c
bntnam <bntnam@users.noreply.github.com>
2020-05-17 00:55:38 +0000

    deploy: b34143964a0e7c8a5cda378fa6b7520861730467

--- component---src-pages-page-2-js-99554c22c48c35769640.js
+++ component---src-pages-page-2-js-99554c22c48c35769640.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),c=t("Wbzz"),l=t("7oih");n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-63f8d0677455d51246ff.js.map
+//# sourceMappingURL=component---src-pages-page-2-js-99554c22c48c35769640.js.map
