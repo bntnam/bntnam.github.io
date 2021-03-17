109223f6d64166bec8f9533d2446a2f224d8db0d
bntnam <bntnam@users.noreply.github.com>
2021-03-17 07:53:31 +0000

    deploy: f80c7bcb79cc3856098be953ec485be53f196e3e

--- component---src-pages-404-js-4a08943ca5604ef22849.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),c=t.n(a),l=t("CcWB");e.default=function(){return c.a.createElement(l.a,null,c.a.createElement("h1",null,"NOT FOUND"))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-4a08943ca5604ef22849.js.map
